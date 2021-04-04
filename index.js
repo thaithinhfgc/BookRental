f198eb0c35198adc4700cba8d5660adde59bdbe8
thaithinh <thaithinhbanzai79@gmail.com>
2021-04-04 15:19:20 +0700

    setup v1

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
 const express = require('express')
+require('express-async-errors')
 const app = express();
-
-
+const auth = require('./routes/auth')
+const categories = require('./routes/categories')
+const rentals = require('./routes/rentals')
+const users = require('./routes/users')
+const app = express()
 
 mongoose.connect('mongodb://localhost/book')
     .then(() => console.log('Connected to MongoDB'))
     .catch(() => console.log('Could not connect to mongoDB'))
+
+app.use(express.json())
+app.use('/api/categories', categories)
+app.use('/api/rentals', rentals)
+app.use('/api/auth', auth)
+app.use('/api/users', users)
+app.use(error)
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => console.log(`Listening on port ${port}`))
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+    let user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Invalid email or password')
+
+})
+
+module.exports = router;
